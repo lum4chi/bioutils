10ae7a333436765843526265b4f49a38cc02683a
lum4chi <lum4chi@gmail.com>
2016-07-21 15:53:20 +0200

    prova

--- bam2sam.sh
+++ bam2sam.sh
@@ -4,11 +4,22 @@
 
 # Given a list of *.bam, convert all in *.sam
 # (by default, search all *.bam in current dir)
-if [[ -z "$@" ]]; then files="*.bam"; else files="$@"; fi
+# if [[ -z "$@" ]]; then files="*.bam"; else files="$@"; fi
+if [[ -z "$@" ]]; then
+  echo 'Usage: bam2sam view [options] <files>'
+fi
 
-echo "Converting $files:";
-for file in $files; do
-        echo " - cooking ${file%.*}..."
-        samtools view $file > ${file%.*}.sam;
-done;
-echo 'Done!';
+opt=()
+
+for o in "$@"; do
+  $opt+=o
+done
+
+echo $opt
+
+# echo "Converting $files:";
+# for file in $files; do
+#   echo " - cooking ${file%.*}..."
+#   samtools view $opt $file > ${file%.*}.sam;
+# done
+echo 'Done!'
